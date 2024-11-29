a3626b6263871e9c339bd871231b89e0b8d3893d
czichy <christian@czichy.com>
2024-11-29 11:46:32 +0100

    update hash

--- flake-parts/cargo.nix
+++ flake-parts/cargo.nix
@@ -11,7 +11,7 @@
     with inputs; let
       rustToolchain = fenix.packages.${system}.fromToolchainFile {
         file = ../rust-toolchain.toml;
-        sha256 = "sha256-pPTJDlL+ul7gVCTO0JPTRhwPSfZse+Zdng5aw9Ukubg=";
+        sha256 = "sha256-pZJWdNhvEsGbBM5yMD3xGi5IaGb01eyRvhCqUVAtFU8=";
       };
 
       pkgs = import nixpkgs {
